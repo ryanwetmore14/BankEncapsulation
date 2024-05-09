f27f9f1ce63cbba301bc9717c96dd685354d2bd4
ryan wetmore <ryanwetmore1014@gmil.com>
2024-05-09 12:20:31 -0400

    completed exercise

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double _balance;
+
+        public void Deposit(double amount)
+        {
+            Console.WriteLine($"Simuating a deposit of {amount,0:c} to your account");
+            _balance = amount;
+        }
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+
+    }
+}
